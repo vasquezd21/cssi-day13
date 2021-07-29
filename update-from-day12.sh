fb5ab32568bf50562c54c09ecea6cddce509c2e1
Dan <dzherr@gmail.com>
2021-07-29 14:15:04 +0000

    add update firebase scripts

---
+++ update-from-day12.sh
@@ -0,0 +1,7 @@
+#Run this script in the terminal anywhere by typing this command
+# sh update-from-day11.sh
+# This assumes you have cloned down both day11 and day12 in your home directory.
+# If that is not the case, you probably know what you are doing and should update it accordingly.
+cp ~/cssi-day12/starter/firebaseNotes/firebase.json ~/cssi-day13/starter/firebaseNotes/firebase.json
+cp ~/cssi-day12/starter/firebaseNotes/database.rules.json ~/cssi-day13/starter/firebaseNotes/database.rules.json
+cp ~/cssi-day12/starter/firebaseNotes/.firebaserc ~/cssi-day13/starter/firebaseNotes/.firebaserc
---
+++ update-to-final.sh
@@ -0,0 +1,5 @@
+#Run this script in the terminal in this directory by typing this command
+# sh update-to-final.sh
+cp starter/firebaseNotes/firebase.json final/firebaseNotes/firebase.json
+cp starter/firebaseNotes/database.rules.json final/firebaseNotes/database.rules.json
+cp starter/firebaseNotes/.firebaserc final/firebaseNotes/.firebaserc
